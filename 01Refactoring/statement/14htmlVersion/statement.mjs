96b1dcc9f5b013da45920d98028f986ae4465a20
divinity6 <divinity666@naver.com>
2022-10-13 20:04:40 +0900

    statement_html_version

--- 01Refactoring/statement/14htmlVersion/statement.mjs
+++ 01Refactoring/statement/14htmlVersion/statement.mjs
@@ -7,6 +7,57 @@ import createStatementData from "./createStatementData.mjs";
  *
  * @return { string } - 청구서 결과를 문자열로 반환합니다
  */
+function statement( invoice , plays ){
+
+    return renderPlainText( createStatementData( invoice , plays ) );
+
+}
+
+/**
+ * - 여기는 정말 UI 에 데이터를 뿌려주는 역할만합니다
+ *
+ * @param { {
+ *      customer : Invoice.customer ,
+ *      performances : Invoice.performances,
+ *      totalAmount : number ,
+ *      totalVolumeCredits : number
+ *
+ * } } data - 중간 데이터 구조입니다
+ *
+ * @return { string } - 청구서 결과를 문자열로 반환합니다
+ */
+function renderPlainText( data ){
+
+    let result = `청구 내역 ( 고객명 : ${ data.customer } )\n`;
+
+    for ( let pref of data.performances ){
+
+        result += `${ pref.play.name } : ${ usd( pref.amount ) } ( ${ pref.audience }석 )\n`;
+
+    }
+
+    result += `총액 : ${ usd( data.totalAmount ) }\n`;
+
+    result += `적립 포인트 : ${ data.totalVolumeCredits }점 \n`;
+
+    return result;
+
+    function usd( aNumber ){
+        return  new Intl.NumberFormat( "en-US" , {
+            style : "currency" , currency : "USD" , minimumFractionDigits : 2
+        } ).format( aNumber / 100 );
+    }
+
+}
+
+
+/**
+ *
+ * @param { Invoice } invoice
+ * @param { { Play } } plays
+ *
+ * @return { string } - 청구서 결과를 문자열로 반환합니다
+ */
 function htmlStatement( invoice , plays ){
 
     /** 중간데이터 생성 함수를 공유합니다 */
