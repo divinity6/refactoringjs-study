43dc0e50b8b5382fd909f6025d87d84ad9f00794
divinity6 <divinity666@naver.com>
2022-11-19 18:30:47 +0900

    [ review ][ 06Basic > RenameVariable ] 변수_이름_바꾸기

---
+++ 05Catalog/6.7_RenameVariable_변수_이름_바꾸기/overview.mjs
@@ -0,0 +1,19 @@
+/**
+ * - 더미 코드
+ */
+
+/** @example */
+function overview( height , width ){
+    let a = height * width;
+}
+
+/**
+ * =============================================================
+ *                          Refactor
+ * =============================================================
+ */
+
+function overview( height , width ){
+    let area = height * width;
+}
+
