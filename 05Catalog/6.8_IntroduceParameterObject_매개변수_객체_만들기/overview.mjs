c592062aa91431795705f1b0c7cd4ba851e408ea
divinity6 <divinity666@naver.com>
2022-11-19 18:44:44 +0900

    [ review ][ 06Basic > IntroduceParameterObject ] 매개변수_객체_만들기

---
+++ 05Catalog/6.8_IntroduceParameterObject_매개변수_객체_만들기/overview.mjs
@@ -0,0 +1,19 @@
+/**
+ * - 더미 코드
+ */
+
+/** @example */
+function amountInvoiced( startDate , endDate ){}
+function amountReceived( startDate , endDate ){}
+function amountOverdue( startDate , endDate ){}
+
+/**
+ * =============================================================
+ *                          Refactor
+ * =============================================================
+ */
+
+function amountInvoiced( aDateRange ){}
+function amountReceived( aDateRange ){}
+function amountOverdue( aDateRange ){}
+
