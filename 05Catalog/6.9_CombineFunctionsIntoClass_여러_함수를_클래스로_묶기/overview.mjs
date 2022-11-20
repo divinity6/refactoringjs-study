dd1d15febb6aee9df26598ff0aa64d1dfa73027c
divinity6 <divinity666@naver.com>
2022-11-20 15:30:42 +0900

    [ review ][ 06Basic > CombineFunctionsIntoClass ] 여러_함수를_클래스로_묶기

---
+++ 05Catalog/6.9_CombineFunctionsIntoClass_여러_함수를_클래스로_묶기/overview.mjs
@@ -0,0 +1,26 @@
+/**
+ * - 더미 코드
+ */
+
+/** @example */
+function base( aReading ){}
+
+function taxableCharge( aReading ){}
+
+function calculateBaseCharge( aReading ){}
+
+/**
+ * =============================================================
+ *                          Refactor
+ * =============================================================
+ */
+
+class Reading {
+
+    base(){}
+
+    taxableCharge(){}
+
+    calculateBaseCharge(){}
+}
+
