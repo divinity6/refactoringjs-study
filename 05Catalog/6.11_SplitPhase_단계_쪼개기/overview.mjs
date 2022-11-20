e07bc39dd6c3e8493ef000625b02dadc187ceb31
divinity6 <divinity666@naver.com>
2022-11-20 16:39:57 +0900

    [ review ][ 06Basic > SplitPhase ] 단계_쪼개기

---
+++ 05Catalog/6.11_SplitPhase_단계_쪼개기/overview.mjs
@@ -0,0 +1,41 @@
+/**
+ * - 더미 코드
+ */
+const priceList = [];
+const orderString = '';
+const order = '';
+
+
+/** @example */
+const orderData = orderString.split( /\s+/ );
+
+const productPrice = priceList[ orderData[ 0 ].split( '-' )[ 1 ] ];
+
+const orderPrice = parseInt( orderData[ 1 ] ) * productPrice;
+
+
+/**
+ * =============================================================
+ *                          Refactor
+ * =============================================================
+ */
+
+const orderRecord = parseOrder( order );
+
+const orderPrice = price( orderRecord , priceList );
+
+function parseOrder( aString ){
+
+    const values = aString.split( /\s+/ );
+
+    return ( {
+        productID : values[ 0 ].split( '-' )[ 1 ],
+
+        quantity : parseInt( values[ 1 ] ),
+    } );
+
+}
+
+function price( order , priceList ){
+    return order.quantity * priceList[ order.productID ];
+}
